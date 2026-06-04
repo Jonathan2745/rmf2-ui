c799e376d616d14abd921d2eceef7c0c2c979fa9
Jonathan2745 <jonathan.joe2745@gmail.com>
2026-06-05 01:27:21 +0800

    feat(backend): added test data to backend
    Signed-off-by: Jonathan2745 <jonathan.joe2745@gmail.com>

--- apps/dashboard/src/pages/dashboard/system/map/map.tsx
+++ apps/dashboard/src/pages/dashboard/system/map/map.tsx
@@ -1,8 +1,22 @@
 // import { SceneViewer } from './components/scene-viewer';a
 import { SceneViewer } from './components/new-scene-viewer';
 
+import { Center, Spinner } from '@chakra-ui/react';
+import { lazy, Suspense } from 'react';
+
+const SceneViewer = lazy(() => import('./components/new-scene-viewer'));
 export function Map() {
-  return <SceneViewer />;
+  return (
+    <Suspense
+      fallback={
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      }
+    >
+      <SceneViewer />
+    </Suspense>
+  );
 }
 
 export default Map;
