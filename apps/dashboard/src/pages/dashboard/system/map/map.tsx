53171a16c5e00d62b5f81cf1fd553fd991a8cc0f
Chen Bainian <chenbn@artc.a-star.edu.sg>
2026-07-08 03:46:37 +0800

    refactor(scene-viewer): add back view control and small gizmo fixes
    Signed-off-by: Chen Bainian <chenbn@artc.a-star.edu.sg>

--- apps/dashboard/src/pages/dashboard/system/map/map.tsx
+++ apps/dashboard/src/pages/dashboard/system/map/map.tsx
@@ -1,15 +1,12 @@
 import { SceneViewer } from './components';
 import { Horizon } from '@rmf2-ui/chakra';
 import Card = Horizon.Card;
-import { SCENE_URL } from './components/constants';
-
-export type LoadState = 'loading' | 'ready' | 'error';
 
 export function Map() {
   return (
     <Card>
       <SceneViewer.Root>
-        <SceneViewer.Viewport3D sceneUrl={SCENE_URL} />
+        <SceneViewer.Viewport3D />
         <SceneViewer.SceneControl />
         <SceneViewer.ViewControl />
         <SceneViewer.RobotStatusPanel />
